da4ec4d67f756c337466da6d5eb85764a7c0a3b5
Cezary Przemysaw Moczulski <c.moczulski@stud.elka.pw.edu.pl>
2022-01-04 04:21:20 +0100

    Modyfikacja implementacji obiektow skladajacych sie na graf w sposob pozwalajacy na precyzyjna definicje relacji rownosci i bardziej zrozumiala, i schematyczna implementacje algorytmow przeszukiwania grafu

--- load_network.py
+++ load_network.py
@@ -1,56 +1,108 @@
 from xml.etree import ElementTree as ET
-from os import path
 
 
 class Node:
     """
 Node of a network.\n
-Contains `id` and a dict `links` indexed by ids of neighbour nodes,
-consisting of tuples in the form of (capacity_of_link, cost_of_link)
+Contains `id` and a list `links` containing ids of links it belongs to.\n
+Two nodes are equal if their `id`s are equal and
+their `links` contain the same ids
     """
-    def __init__(self, id) -> None:
+    def __init__(self, id: int) -> None:
         self.id = id
-        self.links = {}
+        self.links = []
 
-    def add_neighbour(self, neighbour_id, link_capacity, link_cost) -> None:
-        self.links[neighbour_id] = (link_capacity, link_cost)
+    def add_link(self, link_id: int) -> None:
+        if link_id not in self.links:
+            self.links.append(link_id)
+
+    def __eq__(self, other: 'Node') -> bool:
+        if len(self.links) != len(other.links):
+            return False
+
+        for link_id in self.links:
+            if link_id not in other.links:
+                return False
+
+        return self.id == other.id
+
+    def __ne__(self, other: 'Node') -> bool:
+        return not self == other
 
 
 class Link:
     """
 Link between nodes of a network.\n
-Consists of tuple `ends` containing references to both ends of the link,
-`capacity` and `cost`
+Consists of tuple `ends` containing ids of nodes
+on both ends of the link, `capacity` and `cost`\n
+Two links are equal if values of their `id`, `capacity` and `cost` are equal, and
+their `ends` contain the same ids.
     """
-    def __init__(self, end1: Node, end2: Node, capacity: float, cost: float)\
+    def __init__(self, self_id: int, end1_id: int, end2_id: int,
+                 capacity: float, cost: float)\
             -> None:
-        self.ends = (end1, end2)
+        self.id = self_id
+        self.ends = (end1_id, end2_id)
         self.capacity = capacity
         self.cost = cost
+
+    def get_other_end(self, end: int) -> int:
+        if end == self.ends[0]:
+            return self.ends[1]
+        elif end == self.ends[1]:
+            return self.ends[0]
+        raise ValueError('given end is not one of the ends of this link')
+
+    def __eq__(self, other: 'Link') -> bool:
+        return ((self.ends[0] == other.ends[0] and
+                 self.ends[1] == other.ends[1]) or
+                (self.ends[0] == other.ends[1] and
+                 self.ends[1] == other.ends[0])) and\
+               self.capacity == other.capacity and\
+               self.cost == other.cost and\
+               self.id == other.id
+
+    def __ne__(self, other: 'Link') -> bool:
+        return not self == other
 
 
 class Network:
     """
 Network of nodes connected by symmetrical links\n
-Consists of a `nodes` dict containing all nodes of the network, indexed with
-their ids and `links` list containing all links in the network.\n
-Uniqueness of nodes is maintained based on their ids, links - nodes on their
-ends.
+Contains `nodes` and `links` lists containing all
+nodes and links of the network.\n
+Contains `nodes_ids_map` and `links_ids_map` lists, allowing
+to return from internal, numerical ids to original string ids.
     """
     def __init__(self, nodes_ids: list[str],
-                 links_data: list[tuple[str, str, float, float]]) -> None:
-        self.nodes = {}
+                 links_data: list[tuple[str, str, str, float, float]]) -> None:
+        node_id = 0
+        link_id = 0
+        node_id_str_to_int = dict[str, int]()
+        self.nodes_ids_map = []
+        self.links_ids_map = []
+
+        self.nodes = list[Node]()
         for id in nodes_ids:
-            self.nodes[id] = Node(id)
-        self.links = []
-        for end1, end2, capacity, cost in links_data:
-            self.links.append(Link(end1, end2, capacity, cost))
-            self.nodes[end1].add_neighbour(end2, capacity, cost)
-            self.nodes[end2].add_neighbour(end1, capacity, cost)
+            if id not in self.nodes_ids_map:
+                self.nodes_ids_map.append(id)
+                self.nodes.append(Node(node_id))
+                node_id_str_to_int[id] = node_id
+                node_id += 1
+
+        self.links = list[Link]()
+        for id, end1, end2, capacity, cost in links_data:
+            if id not in self.links_ids_map:
+                end1 = node_id_str_to_int[end1]
+                end2 = node_id_str_to_int[end2]
+                self.links_ids_map.append(id)
+                self.links.append(Link(link_id, end1, end2, capacity, cost))
+                self.nodes[end1].add_link(end2)
+                self.nodes[end2].add_link(end1)
 
 
 def parse_xml(path: str)\
-        -> tuple[list[str], list[tuple[str, str, float, float]]]:
+        -> tuple[list[str], list[tuple[str, str, str, float, float]]]:
     """
 Returns a tuple of 2 lists. First list contains list of ids of nodes in the
 network, second - tuple of id_of_node_on_one_end, id_of_node_on_the_other_end,
@@ -68,12 +120,6 @@ capacity_of_link, cost_of_link
         target = link.find('.//{http://sndlib.zib.de/network}target')
         capacity = link.find('.//{http://sndlib.zib.de/network}capacity')
         cost = link.find('.//{http://sndlib.zib.de/network}cost')
-        links_data.append((source.text, target.text,
+        links_data.append((link.get('id'), source.text, target.text,
                            float(capacity.text), float(cost.text)))
     return(nodes_data, links_data)
-
-
-nodes_ids, links_data =\
-    parse_xml(path.normpath(path.join('data', 'network_structure.xml')))
-network = Network(nodes_ids, links_data)
-print(network.nodes['Dortmund'].links)
